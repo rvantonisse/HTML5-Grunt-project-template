196213153749b1ffe58073a113d6431e7717e6d2
RV Antonisse <info@rv-antonisse.nl>
2014-11-25 15:34:37 +0100

    added package.json and added load-grunt-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,8 @@ module.exports = function(grunt) {
   };
   grunt.initConfig(config);
 
-  // Load tasks
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  // Load grunt-tasks
+  require('load-grunt-tasks')(grunt);
 
   // Default task(s).
   grunt.registerTask('develop', ['clean']);
