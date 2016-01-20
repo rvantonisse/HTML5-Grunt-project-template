3277129829ed8869c64dbdaa360412bfa291a684
RV Antonisse <roel.antonisse@gmail.com>
2016-01-20 23:15:54 +0100

    CLEANUP; Assets cleaned

--- source/assets/js/base/base.js
+++ source/assets/js/base/base.js
@@ -1,22 +1,3 @@
-/*! rv-antonisse.nl */
-// Main JavaScript file
-(function () {
-	'use strict';
-
-	var app = {
-		title: '',
-	};
-	function thisIsAnApp (appName) {
-		appName = appName || 'no name';
-		if (!(appName === 'no name')) {
-			appName = 'the name ' + appName;
-		}
-		console.log('This is an app with ' + appName);
-	}
-
-	thisIsAnApp('SuperGrunt');
-
-	return app = {
-		// return the app public methods
-	};
-})();
+/*!
+** Grunt Website Boilerplate
+*/
