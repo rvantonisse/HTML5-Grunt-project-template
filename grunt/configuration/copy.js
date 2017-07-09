1131871f87c905a1280be64d82547908a99784a2
RV Antonisse <roel.antonisse@gmail.com>
2017-07-09 08:19:05 +0200

    Configuration Copy:
    - Order task configuration
    - Add templatestring for assetsDir
    - Reduce media source to one dir

--- grunt/configuration/copy.js
+++ grunt/configuration/copy.js
@@ -4,52 +4,55 @@
  */
 function getConfiguration(grunt) {
 	'use strict';
+
 	return {
 		development: {
 			files: [
-				// css images
+				// Asset images
 				{
 					expand:true,
-					cwd: '<%= cssSourceDir %>images/',
+					cwd: '<%= assetsDir %>images/',
 					src: ['**/*'],
 					dest: 'development/assets/images/'
+				},
+				// Font assets
+				{
+					expand:true,
+					cwd: '<%= assetsDir %>fonts/',
+					src: ['**/*'],
+					dest: 'development/assets/fonts/'
 				},
 				// General media
 				{
 					expand:true,
-					cwd: 'source/assets/media/',
+					cwd: 'source/media/',
 					src: ['**/*'],
 					dest: 'development/media/'
-				},
-				// Views and subviews media
-				{
-					expand:true,
-					cwd: 'source/templates/views/',
-					src: ['**/media/*', '**/**/media/*'],
-					dest: 'development/'
 				}
 			]
 		},
 		distribution: {
 			files: [
+				// Asset images
 				{
 					expand:true,
-					cwd: '<%= cssSourceDir %>images/',
+					cwd: '<%= assetsDir %>images/',
 					src: ['**/*'],
 					dest: 'distribution/assets/images/'
 				},
+				// Font assets
 				{
 					expand:true,
-					cwd: 'source/assets/media/',
+					cwd: '<%= assetsDir %>fonts/',
+					src: ['**/*'],
+					dest: 'distribution/assets/fonts/'
+				},
+				// Media
+				{
+					expand:true,
+					cwd: 'source/media/',
 					src: ['**/*'],
 					dest: 'distribution/media/'
-				},
-				// Views and subviews media
-				{
-					expand:true,
-					cwd: 'source/templates/views/',
-					src: ['**/media/*', '**/**/media/*'],
-					dest: 'distribution/'
 				}
 			]
 		}
